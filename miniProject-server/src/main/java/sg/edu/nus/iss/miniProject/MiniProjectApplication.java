c87f1637049d41a2a0b157db6f1b08d71ba34c2c
chengjia21 <ccj.pepe@protonmail.com>
2023-09-14 06:02:21 +0800

    commit

--- miniProject-server/src/main/java/sg/edu/nus/iss/miniProject/MiniProjectApplication.java
+++ miniProject-server/src/main/java/sg/edu/nus/iss/miniProject/MiniProjectApplication.java
@@ -2,8 +2,9 @@ package sg.edu.nus.iss.miniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MiniProjectApplication {
 
 	public static void main(String[] args) {
